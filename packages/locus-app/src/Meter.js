4d7e2edff18fd9e60ae301a65a7e6e09b0734da0
Ed Perkins <ed@edperkins.co.uk>
2018-09-05 08:51:37 +0100

    feat: create meter and start in app

---
+++ packages/locus-app/src/Meter.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Meter from 'meter';
+
+// pull in graphing/waveform lib
+export default class MeterWrapper extends Component {
+  constructor(props) {
+    super(props);
+
+    this.meter= new Meter()
+  }
+
+  componentWillUnmount() {
+    this.agent.cleanup();
+  }
+
+  render() {
+    return (
+      <div>
+        <pre>
+          {JSON.stringify(this.meter.start(), null, '  ')}
+        </pre>
+      </div>
+    );
+  }
+}
