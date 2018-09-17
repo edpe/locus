3c5d4c1590b3a2a25b05312d30fc4d1b42c4b7ff
edpe <ed@edperkins.co.uk>
2018-09-17 11:03:49 +0100

    feat: add play button

--- packages/locus-app/src/Agent.js
+++ packages/locus-app/src/Agent.js
@@ -1,40 +1,43 @@
 import React, { Component } from "react";
+import play from "./play-button.svg";
 import Agent from "simple-agent";
 
 // pull in graphing/waveform lib
 export default class AgentWrapper extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.agentEd = new Agent();
+
+    this.startTransport = this.startTransport.bind(this);
   }
+
+  startTransport() {
+    console.log("play button gets pressed");
+    console.log(this.agentEd);
+    this.agentEd.startTransport();
+  }
+
+
   componentDidMount() {
-    //this first
-
-
     this.agentEd.addLoop("C2", "8n", "4n"); //note, length, loop interval
     this.agentEd.addChord(["A2", "C3", "E3", "A3"], "16n");
     this.agentEd.addChord(["A3", "C4", "E4", "A4"], "16n");
-    this.agentEd.addPattern(["A4", "B4", "C5", "D5"], "up", "8n"); //notes to play, order to play them in, length of notes
-    this.agentEd.addPattern(["A3", "B3", "C4", "D4"], "up", "8n");
-    console.log(this.agentEd)
+    this.agentEd.addPattern(["C5", "E5", "G5", "C6"], "up", "4n"); //notes to play, order to play them in, length of notes
+    this.agentEd.addPattern(["C5", "G4", "E3", "C4"], "up", "4n");
 
-    //then this after 1000ms
-    setTimeout(function() {
-      console.log("this one", this.agentEd)
-      this.agentEd.startTransport}, 2000)
-
-    //then this next
     this.agentEd.playLoop(0, 0, "4m"); // loop number, start time, end time
     this.agentEd.playChord(0, 0, 4, "1m"); // chord index, start, repeats, length
     this.agentEd.playChord(1, 0, 4, "1m"); // chord index, start, repeats, length
     this.agentEd.playPattern(0, 0, "4m"); // index of patern to play, start point, end point(bars/measures)
     this.agentEd.playPattern(1, 0, "4m"); // index of patern to play, start point, end point(bars/measures)
-
   }
 
   render() {
     return (
       <div>
+        <button>
+          <img src={play} alt="play" onClick={this.startTransport} />
+        </button>
         <pre />
       </div>
     );
