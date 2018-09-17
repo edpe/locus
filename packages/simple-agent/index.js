50f2d4398ce47c2a5a282b591ef0b0421b2f2328
edpe <ed@edperkins.co.uk>
2018-09-17 08:59:32 +0100

    feat: agent plays chords

--- packages/simple-agent/index.js
+++ packages/simple-agent/index.js
@@ -1,28 +1,45 @@
-const { Synth, Loop, PolySynth, Pattern } = require("tone");
+const {
+  Synth,
+  Loop,
+  PolySynth,
+  MembraneSynth,
+  MetalSynth,
+  Pattern,
+  Event,
+  Part
+} = require("tone");
 
-require("tone").Transport.start(0);
-
-const polySynth = new PolySynth(6, Synth).toMaster();
+const polySynth = new PolySynth(8, Synth).toMaster();
+const synth = new MembraneSynth().toMaster();
+const cymbal = new MetalSynth().toMaster();
 
 class Agent {
   constructor(options) {
     this.instrument = polySynth;
     this.loops = [];
     this.patterns = [];
+    this.chords = [];
   }
+
+  startTransport() {
+    require("tone").Transport.bpm.value = 160;
+      require("tone").Transport.start(0);
+    }
 
   addLoop(note, length, loopInterval) {
     this.loops.push(
       new Loop(function(time) {
-        polySynth.triggerAttackRelease(note, length, time);
+        console.log("loop", note, time);
+        synth.triggerAttackRelease(note, length, time, 0.5);
       }, loopInterval)
     );
   }
 
-  addPattern(notes, order, length) {
+  addPattern(notes, order, length, rate) {
     this.patterns.push(
       new Pattern(
         function(time, note) {
+          console.log("pattern", note, time);
           polySynth.triggerAttackRelease(note, length);
         },
         notes,
@@ -31,17 +48,42 @@ class Agent {
     );
   }
 
+  addChord(chordNotes, length) {
+    this.chords.push(
+      new Event(function(time, note) {
+        console.log("chord", time, note);
+        polySynth.triggerAttackRelease(note, "1n", time,  0.5);
+      }, chordNotes)
+
+    );
+      console.log(this.chords)
+  }
+
   playPattern(patternIndex, start, end) {
     this.patterns[patternIndex].start(start).stop(end);
-    console.log(this.patterns[patternIndex]);
   }
 
   playLoop(loopIndex, start, end) {
     this.loops[loopIndex].start(start).stop(end);
   }
 
-  cleanup() {
-    console.log("ive removed stuff");
+  playChord(chordIndex, start, repeats, length) {
+    this.chords[chordIndex].loop = repeats;
+    this.chords[chordIndex].loopEnd = length;
+    this.chords[chordIndex].start();
+  }
+
+  logTransport1() {
+    require("tone").Transport.scheduleRepeat(function(time) {
+      console.log("logging transport1", time);
+      cymbal.triggerAttackRelease();
+    }, "4n");
+  }
+
+  logTransport2() {
+    require("tone").Transport.scheduleRepeat(function(time) {
+      console.log("logging transport2", time);
+    }, "8n");
   }
 }
 
